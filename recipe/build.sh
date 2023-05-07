d8a7812fcfe24da1fd38fda7dd4e7bb35c46bf67
Kerry Su <me@sshockwave.net>
2023-05-08 00:49:14 +0800

    Add binaries for win64, osx-64, and osx-arm64

---
+++ recipe/build.sh
@@ -0,0 +1,28 @@
+set -eoux pipefail
+
+# Set the correct version
+echo '{"version": "'"${PKG_VERSION}"'"}' > version.json
+
+# Build app
+if [[ "${target_platform}" == linux-* ]]
+then
+  dotnet build ./src/linux/Packaging.Linux/Packaging.Linux.csproj \
+    -c Release \
+    -p:InstallFromSource=true \
+    ;
+else
+  # Install script taken from
+  # https://github.com/git-ecosystem/git-credential-manager/blob/release/src/linux/Packaging.Linux/build.sh
+
+  INSTALL_TO="$PREFIX/share/gcm-core/"
+  LINK_TO="$PREFIX/bin/"
+
+  mkdir -p "$INSTALL_TO" "$LINK_TO"
+  cp -R payload/* "$INSTALL_TO"
+
+  # Create symlink
+  ln -s "$INSTALL_TO/git-credential-manager" "$LINK_TO/git-credential-manager"
+
+  # Create legacy symlink with older name
+  ln -s "$INSTALL_TO/git-credential-manager" "$LINK_TO/git-credential-manager-core"
+fi
