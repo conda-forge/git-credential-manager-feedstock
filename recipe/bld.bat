4e281074b18dd9173329771ddb13f20d63d09b8c
Kerry Su <me@sshockwave.net>
2023-05-07 21:45:23 +0800

    Add dotnet tools package

---
+++ recipe/bld.bat
@@ -0,0 +1,4 @@
+dotnet tool install git-credential-manager ^
+  --tool-path %DOTNET_TOOLS% ^
+  --add-source %SRC_DIR% ^
+  --verbosity diagnostic
---
+++ recipe/build.sh
@@ -0,0 +1,17 @@
+set -eoux pipefail
+
+if [[ "${target_platform}" == "linux-64" ]]
+then
+  # Set correct install location
+  sed -i -E 's|^\s*INSTALL_LOCATION="(.*)"$|    INSTALL_LOCATION="${PREFIX}"|g' ./src/linux/Packaging.Linux/build.sh
+  # Set the correct version
+  echo $(jq ".version = \"${PKG_VERSION}}\"" version.json) > version.json
+  # Build app
+  dotnet build ./src/linux/Packaging.Linux/Packaging.Linux.csproj -c Release -p:InstallFromSource=true
+else
+  dotnet tool install git-credential-manager \
+    --tool-path "${DOTNET_TOOLS}" \
+    --add-source "${SRC_DIR}" \
+    --verbosity diagnostic \
+    ;
+fi
