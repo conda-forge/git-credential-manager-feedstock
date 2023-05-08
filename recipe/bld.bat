08f30ec8f4fd7f2187e0aad07054eddfaa59bbf0
Kerry Su <me@sshockwave.net>
2023-05-08 11:35:03 +0800

    Build on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,29 +1,30 @@
+pushd src\windows\Installer.Windows\
+if errorlevel 1 exit /b %errorlevel%
+
+powershell -Command ".\layout.ps1 -Configuration WindowsRelease -Output payload -SymbolOutput symbols"
+if errorlevel 1 exit /b %errorlevel%
+
+popd
+
+move src\shared\DotnetTool\DotnetToolSettings.xml payload\
+if errorlevel 1 exit /b %errorlevel%
+
+move src\shared\DotnetTool\icon.png payload\
+if errorlevel 1 exit /b %errorlevel%
+
+rem Pack into dotnet tool such that the dlls won't spill into the %PREFIX%
+dotnet pack src\shared\DotnetTool\DotnetTool.csproj ^
+  /p:Configuration=Release ^
+  /p:PackageVersion=%PKG_VERSION% ^
+  /p:PublishDir=payload ^
+  --runtime win-x64 ^
+  --output
+if errorlevel 1 exit /b %errorlevel%
+
 dotnet tool install git-credential-manager ^
   --tool-path %PREFIX%\dotnet\tools ^
-  --add-source %SRC_DIR% ^
-  --verbosity diagnostic
-if errorlevel neq 0 exit /b %errorlevel%
+  --add-source out\shared\DotnetTool\nupkg\Release
+if errorlevel 1 exit /b %errorlevel%
 
-cd %PREFIX%\dotnet\tools\.store\%PKG_NAME%\%PKG_VERSION%\%PKG_NAME%\%PKG_VERSION%
-if errorlevel neq 0 exit /b %errorlevel%
-
-del %PKG_NAME%.%PKG_VERSION%.nupkg
-if errorlevel neq 0 exit /b %errorlevel%
-
-cd tools\net6.0\any\runtimes
-if errorlevel neq 0 exit /b %errorlevel%
-
-move win-x64 ..
-if errorlevel neq 0 exit /b %errorlevel%
-
-move win ..
-if errorlevel neq 0 exit /b %errorlevel%
-
-del /s *
-if errorlevel neq 0 exit /b %errorlevel%
-
-move ..\win-x64 .
-if errorlevel neq 0 exit /b %errorlevel%
-
-move ..\win .
-if errorlevel neq 0 exit /b %errorlevel%
+del %PREFIX%\dotnet\tools\.store\%PKG_NAME%\%PKG_VERSION%\%PKG_NAME%\%PKG_VERSION%\%PKG_NAME%.%PKG_VERSION%.nupkg
+if errorlevel 1 exit /b %errorlevel%
