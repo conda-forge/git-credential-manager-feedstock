f94e13521620fd8374f75e2a74dd113a9f0ff1cd
Kerry Su <me@sshockwave.net>
2023-05-08 10:10:18 +0800

    Remove extra artifacts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,13 @@ dotnet tool install git-credential-manager ^
   --verbosity diagnostic
 if errorlevel neq 0 exit /b %errorlevel%
 
-cd %PREFIX%\dotnet\tools\.store\%PKG_NAME%\%PKG_VERSION%\%PKG_NAME%\%PKG_VERSION%\tools\net6.0\any\runtimes
+cd %PREFIX%\dotnet\tools\.store\%PKG_NAME%\%PKG_VERSION%\%PKG_NAME%\%PKG_VERSION%
+if errorlevel neq 0 exit /b %errorlevel%
+
+del %PKG_NAME%.%PKG_VERSION%.nupkg
+if errorlevel neq 0 exit /b %errorlevel%
+
+cd tools\net6.0\any\runtimes
 if errorlevel neq 0 exit /b %errorlevel%
 
 mv win-x64 ..
